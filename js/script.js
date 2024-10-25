2b20d4e05b7bd1d83f69c10e269a2bf862b92c2b
Danilo <danilo.tavares@coderp.inf.br>
2024-10-25 20:38:34 -0300

    Arquivo js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,62 @@
-const pokemonName = document.querySelector(".pokemon_name")
-const pokemonNumber = document.querySelector(".pokemon_number")
-const pokemonImage = document.querySelector(".pokemon_image")
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImage = document.querySelector(".pokemon_image");
+
+var form = document.querySelector(".form");
+var input = document.querySelector(".input_search");
 
 
-var form = document.querySelector(".form")
-var input = document.querySelector(".input_search")
+var buttonext = document.querySelector(".button.btn-next");
+var buttonprev = document.querySelector(".button.btn-prev")
 
-
-var buttonext = document.querySelector(".button btn-next")
-
+let currentPokemonId = 1;
 
 const fetchPokemon = async (pokemon) => {
-    // é uma função asyncrona e espera essa função ser executada para os outros codigos serem executados
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    if(APIResponse.status === 200){
-
+    if (APIResponse.status === 200) {
         const data = await APIResponse.json();
-    
         return data;
-        //extraindo dados json
-            
     }
-
 }
-
-//renderizar os dados na tela
 
 const renderPokemon = async (pokemon) => {
-
-    pokemonName.innerHTML = "Loading..."
-    pokemonNumber.innerHTML = ""
+    pokemonName.innerHTML = "Loading...";
+    pokemonNumber.innerHTML = "";
 
     const data = await fetchPokemon(pokemon);
-   if(data){
-    pokemonNumber.innerHTML = data.id
-    pokemonName.innerHTML = data.name
-    
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
-    
-    console.log(data)
-     input.value = ''
-   }else {
-    pokemonName.innerHTML = "Not found :c"
-    pokemonNumber.innerHTML = ""
-   }
+    if (data) {
+        pokemonNumber.innerHTML = data.id;
+        pokemonName.innerHTML = data.name;
+        pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        console.log(data);
+        input.value = '';
+        currentPokemonId = data.id; // Atualiza o ID do Pokémon atual
+    } else {
+        pokemonName.innerHTML = "Not found :c";
+        pokemonNumber.innerHTML = "";
+    }
 }
 
-renderPokemon('1')
+renderPokemon(1);
 
-
-
-form.addEventListener('submit', (event)=>{
-
-    
+form.addEventListener('submit', (event) => {
     event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
 
-    renderPokemon(input.value.toLowerCase())
-   
+buttonext.addEventListener("click", (event) => {
+    currentPokemonId++;
+    renderPokemon(currentPokemonId);
+});
+
+buttonprev.addEventListener("click", (event) =>{
+    if(currentPokemonId > 1){
+        currentPokemonId--;
+        renderPokemon(currentPokemonId)
+    }
+    
 })
 
-buttonext.addEventListener("click", (event)=>{
+console.log("Fetching Pokemon:", data.name);
+console.log("Fetched data:", data);
 
-    let next = 1;
-    renderPokemon( data.id + 1)
-})
